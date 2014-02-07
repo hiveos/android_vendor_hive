da82e496502cde7f4d5bab38d36106b8433f14cb
hikari <x.ihsan@gmail.com>
2014-02-07 22:38:26 +0100

    Add prebuilt Google Keyboard to build, rename bootanimation package...

--- configs/system.mk
+++ configs/system.mk
@@ -4,13 +4,18 @@ BOARD := $(subst hive_,,$(TARGET_PRODUCT))
 #PRODUCT_PACKAGE_OVERLAYS += vendor/hive/overlay/common
 #PRODUCT_PACKAGE_OVERLAYS += vendor/hive/overlay/$(TARGET_PRODUCT)
 
-HIVE_VERSION_MAJOR = 0
-HIVE_VERSION_MINOR = 1
+# Gesture enabled JNI
+PRODUCT_COPY_FILES += \
+    vendor/hive/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 
 #Temporary packages for device navigation 
 PRODUCT_PACKAGES := \
     GoogleHome \
     GoogleNow
+
+#Other packages
+PRODUCT_PACKAGES := \
+    GoogleKeyboard
 
 VERSION := $(HIVE_VERSION_MAJOR).$(HIVE_VERSION_MINOR)
 ifeq ($(DEVELOPER_VERSION),true)
